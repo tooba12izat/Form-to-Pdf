f192ffdb880cae570eb59b78dee35e69ee6f9c36
tooba12izat <tooba12izat@gmail.com>
2024-04-02 13:02:01 +0500

    lastest

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import FinancialStatement13 from './Forms/FinancialStatement13';
 import { Route, Link, Routes } from 'react-router-dom';
-
 import ApplicationDivorce8A from './Forms/ApplicationDivorce8A';
 import FinancialStatement131 from './Forms/FinancialStatement131';
 import NetFamilyPropertyStatement13B from './Forms/NetFamilyPropertyStatement13B';
 import CertificateOfFinancialDisclosure13A from './Forms/CertificateOfFinancialDisclosure13A'
-import Navbar from "./Navbar"
-
+import Navbar from './Navbar'
 function App() {
   return (
     <>
