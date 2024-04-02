6b56f37f0322426c182419eb2cbfe54a6412bbd4
tooba12izat <tooba12izat@gmail.com>
2024-04-02 12:52:12 +0500

    changes reverted

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import FinancialStatement13 from './routes/FinancialStatement13';
+import FinancialStatement13 from './Forms/FinancialStatement13';
 import { Route, Link, Routes } from 'react-router-dom';
 
-import ApplicationDivorce8A from './routes/ApplicationDivorce8A';
-import FinancialStatement131 from './routes/FinancialStatement131';
-import NetFamilyPropertyStatement13B from './routes/NetFamilyPropertyStatement13B';
-import CertificateOfFinancialDisclosure13A from './routes/CertificateOfFinancialDisclosure13A'
+import ApplicationDivorce8A from './Forms/ApplicationDivorce8A';
+import FinancialStatement131 from './Forms/FinancialStatement131';
+import NetFamilyPropertyStatement13B from './Forms/NetFamilyPropertyStatement13B';
+import CertificateOfFinancialDisclosure13A from './Forms/CertificateOfFinancialDisclosure13A'
 import Navbar from "./Navbar"
 
 function App() {
   return (
     <>
-
+      <Navbar />
 
       <Routes>
         <Route path="/CertificateOfFinancialDisclosure13A" element={<CertificateOfFinancialDisclosure13A />} />
-        <Route path="/" element={<Navbar />} />
         <Route path="/NetFamilyPropertyStatement13B" element={<NetFamilyPropertyStatement13B />} />
         <Route path="/FinancialStatement131" element={<FinancialStatement131 />} />
         <Route path="/ApplicationDivorce8A" element={<ApplicationDivorce8A />} />
